c765f313d5804d3c86c2f8c58d537d483b584ce1
oguz <oguzkayaya@gmail.com>
2020-09-30 12:06:48 +0300

    save, update and delete authors

--- src/main/java/com/ok/libraryapp/controllers/AuthorController.java
+++ src/main/java/com/ok/libraryapp/controllers/AuthorController.java
@@ -4,6 +4,7 @@ import com.ok.libraryapp.models.Author;
 import com.ok.libraryapp.services.AuthorService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -24,4 +25,36 @@ public class AuthorController {
         model.addAttribute("authors", authorList);
         return "/author/authorList";
     }
+
+    @RequestMapping(value = "/authors/new", method = RequestMethod.GET)
+    public String showAuthorForm() {
+        return "/author/authorForm";
+    }
+
+    @RequestMapping(value = "/authors", method = RequestMethod.POST)
+    public String addAuthor(Author author, Model model) {
+        authorService.saveAuthor(author);
+        return "redirect:/authors";
+    }
+
+    @RequestMapping(value = "/authors/delete/{id}", method = RequestMethod.GET)
+    public String deleteAuthor(@PathVariable int id) {
+        authorService.deleteAuthor(id);
+        return "redirect:/authors";
+    }
+
+    @RequestMapping(value = "/authors/update/{id}", method = RequestMethod.GET)
+    public String showUpdateForm(@PathVariable int id, Model model) {
+        Author author = authorService.getAuthor(id);
+        model.addAttribute("updateAuthor", author);
+        return "/author/authorUpdateForm";
+    }
+
+    @RequestMapping(value = "/authors/update/{id}", method = RequestMethod.POST)
+    public String updateAuthor(@PathVariable int id, Author newAuthor) {
+        authorService.updateAuthor(newAuthor, id);
+        return "redirect:/authors";
+    }
+
+
 }
--- src/main/java/com/ok/libraryapp/services/AuthorService.java
+++ src/main/java/com/ok/libraryapp/services/AuthorService.java
@@ -18,4 +18,26 @@ public class AuthorService {
     public List<Author> getAuthors() {
         return authorRepository.findAll();
     }
+
+    public void saveAuthor(Author author) {
+        authorRepository.save(author);
+    }
+
+    public void deleteAuthor(int id) {
+        authorRepository.deleteById(id);
+    }
+
+    public void updateAuthor(Author author, int id) {
+        Author a = getAuthor(id);
+        a.setName(author.getName());
+        a.setDescription(author.getDescription());
+        a.setBirthDate(author.getBirthDate());
+        a.setGender(author.getGender());
+        authorRepository.save(a);
+    }
+
+    public Author getAuthor(int id){
+        return authorRepository.findById(id).get();
+    }
+
 }
